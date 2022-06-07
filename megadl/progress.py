38ba4fcf546080450453569a28e46c1e2d199a97
BlackHatDevX <91268029+BlackHatDevX@users.noreply.github.com>
2022-06-07 12:51:51 +0530

    Update progress.py

--- megadl/progress.py
+++ megadl/progress.py
@@ -46,7 +46,7 @@ async def progress_for_pyrogram(
         )
         try:
             await message.edit(
-                text="**{}** {} \n\n**@deveIoper_X**".format(
+                text="Jash Gro".format(
                     ud_type,
                     tmp
                 ),
